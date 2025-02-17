97671010e31410f6e3a366d128d026fa95952334
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-17 11:11:24 +0300

    Файл конфигурации версии расширения

---
+++ config/version.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Файл конфигурации версии расширения.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    'name'        => 'Configuring access by IP Address',
+    'description' => 'Restricting user access to the control panel and website by IP address',
+    'version'     => '1.0',
+    'versionDate' => '20-12-2017',
+    'author'      => 'GearMagic',
+    'authorUrl'   => 'https://gearmagic.ru',
+    'email'       => 'anton.tivonenko@gmail.com',
+    'url'         => 'https://apps.gearmagic.ru/component/gm-be-config-defense',
+    'license'     => 'The MIT License',
+    'licenseUrl'  => 'https://apps.gearmagic.ru/license/mit-license.html'
+];
